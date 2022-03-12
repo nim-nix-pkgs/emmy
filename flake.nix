1f7d0c63813a783182aa29a47642c932be60495a
hugosenari <hugosenari@gmail.com>
2022-03-12 02:02:35 -0300

    chore: re index emmy

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''Algebraic structures and related operations for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."emmy-master".url = "path:./master";
+  inputs."emmy-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."emmy-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."emmy-0_1_0".url = "path:./0_1_0";
+  inputs."emmy-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."emmy-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."emmy-0_2_0".url = "path:./0_2_0";
+  inputs."emmy-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."emmy-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = ''Algebraic structures and related operations for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-emmy-master.flake = false;
+  inputs.src-emmy-master.owner = "andreaferretti";
+  inputs.src-emmy-master.ref   = "refs/heads/master";
+  inputs.src-emmy-master.repo  = "emmy";
+  inputs.src-emmy-master.type  = "github";
+  
+  inputs."bigints".dir   = "nimpkgs/b/bigints";
+  inputs."bigints".owner = "riinr";
+  inputs."bigints".ref   = "flake-pinning";
+  inputs."bigints".repo  = "flake-nimble";
+  inputs."bigints".type  = "github";
+  inputs."bigints".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bigints".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."stint".dir   = "nimpkgs/s/stint";
+  inputs."stint".owner = "riinr";
+  inputs."stint".ref   = "flake-pinning";
+  inputs."stint".repo  = "flake-nimble";
+  inputs."stint".type  = "github";
+  inputs."stint".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."stint".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-emmy-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-emmy-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
